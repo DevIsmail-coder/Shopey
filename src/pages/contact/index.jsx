2335932df87de516cc13ca07bc4c57b064c039d9
perpetual <zeeoneperppy@gmail.com>
2025-03-17 14:41:27 +0100

    Contactbranch (#4)
    * updated the contact page
    
    * updated the contact page
    
    * updated the contact page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,23 +1,92 @@
 import React from 'react'
 import './contact.css'
+import { CgDanger } from "react-icons/cg";
+import { TfiHome } from "react-icons/tfi";
+import { FaTabletAlt } from "react-icons/fa";
+import { TfiEmail } from "react-icons/tfi";
+
+
+
+
 
 const Contact = () => {
-  return (
-    <div className='contactBody'>
-      <div className='contactHead'>
-        <p className='salesP'>Sale Up To 50% Biggest Discounts. Hurry! Limited Period Offer </p>
-        <p className='shopNow'>Shop Now </p>
-      </div>
+    return (
+        <div className='contactBody'>
+          
+            <div className='contactMiddle'>
+                <h2 className='middleName1'>Contact</h2>
+                <div className='middlepMan'>
+                    <p>Home</p>
+                    <span>|</span>
+                    <p>Contact</p>
 
-      <div className='contactMiddle'>
-          <h2>Contact</h2>
-          <div>
-            <p>Home</p>
-            <p>Contact</p>
-          </div>
-      </div>
-    </div>
-  )
+                </div>
+            </div>
+
+
+            <div className='mapBody'>
+                <div className='mapItself'>
+                    <CgDanger size={50} color='gray' />
+                    <h2 className='oopsDiv'>Oops! Something went wrong</h2>
+                    <p>This page didn't load Google Maps correctly. See the JavaScript console for technical details.</p>
+
+                </div>
+
+            </div>
+
+
+
+            <div className='lastDiv'>
+                <div className='lastDivBox'>
+
+                    <div className='lastDivquarters'>
+                        <h1>Get in touch</h1>
+                        <textarea name="" id="Enter Message"></textarea>
+
+                        <div className='inputHold'>
+                            <input className='nameHold' type="text" placeholder='Enter your name' />
+                            <input className='nameHold' type="text" placeholder='Email' />
+                        </div>
+                        <div className='inputHoldsubject'>
+                            <input className='subjectHold' type="text" placeholder='Email' />
+                        </div>
+
+                        <button className='sendClick'>SEND </button>
+                    </div>
+
+                    <div className='lastdivRight'>
+                        <div className='addressDiv'>
+                            <div className='boxBox'>
+                                <TfiHome size={30} color='gray' />
+                                <div className='pBox'>
+                                <p className='topTier'> Buttonwood, California.</p>
+                                <p className='bottomTier'> Rosemead, CA 91770 </p>
+                                </div>
+                            </div>
+
+                            <div className='boxBox'>
+                                <FaTabletAlt size={30} color='gray' />
+                               <div className='pBox'>
+                               <p className='topTier'>+1 253 565 2365 </p>
+                               <p className='bottomTier'>Mon to Fri 9am to 6pm </p>
+                               </div>
+                            </div>
+                            <div className='boxBox'>
+                                <TfiEmail size={30} color='gray' />
+                                <div className='pBox'>
+                                <p className='topTier'>support@colorlib.com</p>
+                                <p className='bottomTier'>Send us your query anytime!</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Contact
