f2b4aaec8a10060813056d56f77065506e230c7e
DevIsmail-coder <ismailolawale777@gmail.com>
2025-03-24 09:33:51 +0100

    done with the details pages

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
             element: <Baby/>
           },
           {
-            path: "/detailspage",
+            path: "/detailspage/:id",
             element: <Details />
           },
           {
